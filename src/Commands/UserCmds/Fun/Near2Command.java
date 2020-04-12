c2a87d825e356b7b3471bf7bd1255085d2f14b9c
kerlab <thebowers89@gmail.com>
2020-04-12 18:27:54 -0400

    added Near2Command just cause i could

---
+++ src/Commands/UserCmds/Fun/Near2Command.java
@@ -0,0 +1,50 @@
+package Commands.UserCmds.Fun;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import utils.Utils;
+
+import java.util.ArrayList;
+
+import static Main.mainClass.messages;
+
+public class Near2Command implements CommandExecutor {
+
+    private double radius = 50;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.hasPermission("Near") || player.isOp()) {
+                boolean found = false;
+                ArrayList<String> nearbyPlayers = new ArrayList<>();
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    double distance = player.getLocation().distance(target.getLocation());
+                    if (distance <= radius) {
+                        found = true;
+                        nearbyPlayers.add("    " + target.getName() + ": " + distance + "m");
+                    }
+                }
+                if (found) {
+                    player.sendMessage(Utils.chat("List of nearby players:"));
+                    for (String s : nearbyPlayers) {
+                        player.sendMessage(Utils.chat(s));
+                    }
+                } else {
+                    player.sendMessage("No players are nearby!");
+                }
+                return true;
+            } else {
+                player.sendMessage(Utils.chat(messages.getString("NoPerms")));
+            }
+            return true;
+        } else {
+            sender.sendMessage(Utils.chat(messages.getString("SenderNotPlayer")));
+        }
+        return false;
+    }
+}
