cafeff8560dc3bf871ffcc38805528c9117a992e
Kerlab <thebowers89@gmail.com>
2020-04-25 00:13:53 -0400

    Added some comments, that is all

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -16,10 +16,13 @@ public class Utils {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 
+    // Sends messages to a player directly and makes the 'chat' name make more sense
     public static void chat(Player player, String string) {
         player.sendMessage(chat(string));
     }
 
+    // An overload so you can do the same thing when you need to send a message to console
+    // from inside a command class
     public static void chat(CommandSender sender, String string) {
         sender.sendMessage(chat(string));
     }
@@ -29,6 +32,8 @@ public class Utils {
         player.teleport(location);
     }
 
+    // This teleport method lets you send a message to the player here instead of
+    // having to do it where ever you called this method
     public static void teleport(Player player, Location location, String messagePath) {
         backHandler.processPlayer(player);
         player.teleport(location);
